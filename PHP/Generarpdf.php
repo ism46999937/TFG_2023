7fa59b1002206e26b925f7761bda6e99fe315f57
RaulAT03 <Rarcostorralbo@gmail.com>
2023-05-06 11:15:22 +0200

    Feat(GenerarPDF): Codigos de Barras
    He añadido los códigos de barras al pdf

--- PHP/Generarpdf.php
+++ PHP/Generarpdf.php
@@ -1,64 +1,55 @@
 <?php
 	require 'fpdf/fpdf.php';
-	include 'barcode copy.php';
+	include 'Barcode.php';
 
 	$conexion = mysqli_connect("localhost", "root", "root", "TFG_Grupo15");
 
-    $result = mysqli_query($conexion, "select * from Recetas");
+    $result = mysqli_query($conexion, "select * from Temporal");
 	
 	$pdf = new FPDF();
 	$pdf->AddPage();
 	$pdf->SetAutoPageBreak(true, 20);
 	$y = $pdf->GetY();
+
 	$pdf->SetFont('Arial','B',18);
+    $pdf->Cell(0,10,'Receta Medica',0,1,'C');
+    $pdf->Ln(15);
 
-// Escribir el título del informe
-$pdf->Cell(0,10,'Receta Medica',0,1,'C');
-$pdf->Ln(15);
+    // Establecer el color de fondo y de borde de las celdas de la tabla
+    $pdf->SetFillColor(233, 229, 235); // color de fondo rgba
+    $pdf->SetDrawColor(61, 61, 61); // color de la linea
 
-// Establecer el color de fondo y de borde de las celdas de la tabla
-$pdf->SetFillColor(233, 229, 235); // color de fondo rgba
-$pdf->SetDrawColor(61, 61, 61); // color de la linea
+    $pdf->SetFont('Arial','B',10);
+    $pdf->Cell(40,8,'Nombre',1,0,'C',true);
+    $pdf->Cell(30,8,'DNI',1,0,'C',true);
+    $pdf->Cell(40,8,'Nombre Medicamento',1,0,'C',true);
+    $pdf->Cell(40,8,'CB Medicamento',1,0,'C',true);
+    $pdf->Cell(40,8,'CB Receta',1,0,'C',true);
+    $pdf->Ln();
 
-// Escribir el encabezado de la tabla
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(40,8,'Nombre',1,0,'C',true);
-$pdf->Cell(30,8,'DNI',1,0,'C',true);
-$pdf->Cell(40,8,'Nombre Medicamento',1,0,'C',true);
-$pdf->Cell(40,8,'CB Medicamento',1,0,'C',true);
-$pdf->Cell(40,8,'CB Receta',1,0,'C',true);
-$pdf->Ln();
+    $pdf->SetFont('Arial','',9);
 
-// Escribir los datos de la tabla
-$pdf->SetFont('Arial','',9);
+    while($fila = mysqli_fetch_assoc($result)) {
 
-// Descargar la imagen y añadirla al PDF
+		$code = $fila['CodigodeBarrasReceta'];
 
-while($fila = mysqli_fetch_assoc($result)) {
-
-    $pdf->Cell(40,10,$fila['Nombre'],1,0,'C',false);
-    $pdf->Cell(30,10,$fila['DNIPaciente'],1,0,'C',false);
-    $pdf->Cell(40,10,$fila['Nombre_Medicamento'],1,0,'C',false);
-
-
-    $pdf->Cell(40,10,$fila['CodigodeBarrasReceta'],1,0,'C',false);
-
-    $code = $row['CodigodeBarrasMed'];		
-    barcode('codigos/'.$code.'.png', $code, 20, 'horizontal', 'code128', true);
-    $pdf->Image('codigos/'.$code.'.png',10,50,0,'PNG');
-    
-
-}
-	while ($row = $result->fetch_assoc()){
-		
-		$code = $row['CodigodeBarrasMed'];
-		
 		barcode('codigos/'.$code.'.png', $code, 20, 'horizontal', 'code128', true);
-		
-		$pdf->Image('codigos/'.$code.'.png',10,$y,50,0,'PNG');
-		
-		$y = $y+15;
-	}
+
+		$code2 = $fila['CodigodeBarrasMed'];
+
+		barcode('codigos/'.$code2.'.png', $code2, 20, 'horizontal', 'code128', true);
+
+        $pdf->Cell(40,10,$fila['Nombre'],1,0,'C',false);
+        $pdf->Cell(30,10,$fila['DNIPaciente'],1,0,'C',false);
+        $pdf->Cell(40,10,$fila['Nombre_Medicamento'],1,0,'C',false);
+        $pdf->Image('codigos/'.$code2.'.png',$pdf->GetX(),$pdf->GetY()+2,40,0,'PNG');
+        $pdf->Cell(40,10,'',1,0,'C',false);
+        $pdf->Image('codigos/'.$code.'.png',$pdf->GetX(),$pdf->GetY()+2,40,0,'PNG');
+        $pdf->Cell(40,10,"",1,0,'C',false);
+        $pdf->ln();
+
+    
+    }
 	$pdf->Output();	
 	
 ?>
